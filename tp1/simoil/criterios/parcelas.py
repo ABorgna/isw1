cc34ea25af5793af90aa6750c2588d9932d23f55
Gonzalo Benegas <gsbenegas@gmail.com>
2017-05-27 20:36:09 -0300

    Evito un bug con nueva parametrizacion

--- tp1/simoil/criterios/parcelas.py
+++ tp1/simoil/criterios/parcelas.py
@@ -12,19 +12,13 @@ class CriterioEleccionParcelas(metaclass=ABCMeta):
 class EleccionParcelasMayorPresion(CriterioEleccionParcelas):
     def __init__(self, n_parcelas):
         self.n_parcelas = int(n_parcelas)
-        self.n_parcelas_seleccionadas = 0
 
     def decidir_proximas_parcelas(self, estado):
-        if self.n_parcelas_seleccionadas == self.n_parcelas:
-            return
-
-        parcelas_seleccionadas = sorted(
+        if estado.diaNumero == 1:
+            parcelas_seleccionadas = sorted(
                 estado.yacimiento.parcelasDelYacimiento,
                 key=lambda parcela: parcela.presionInicial, reverse=True) \
-            [:self.n_parcelas]
+                [:self.n_parcelas]
 
-        self.n_parcelas_seleccionadas = len(parcelas_seleccionadas)
-
-        for parcela in parcelas_seleccionadas:
-            estado.agregarExcavacion(Excavacion(parcela))
-
+            for parcela in parcelas_seleccionadas:
+                estado.agregarExcavacion(Excavacion(parcela))
