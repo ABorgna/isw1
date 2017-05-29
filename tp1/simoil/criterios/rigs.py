9fb6fe0f78282c3bf8b65a7160b0363d2bf9787c
Agustin Borgna <hello@aborgna.com.ar>
2017-05-29 01:53:24 -0300

    criterio extra de rigs

--- tp1/simoil/criterios/rigs.py
+++ tp1/simoil/criterios/rigs.py
@@ -72,3 +72,68 @@ class CriterioContratacionYUsoDeRigsMinimoTiempo(CriterioContratacionYUsoDeRigs)
                     estado.agregarPozo(nuevo_pozo)
 
             estado.excavacionesActuales = [excavacion for excavacion in estado.excavacionesActuales if not excavacion.termino]
+
+
+class CriterioContratacionYUsoDeRigsMinimoCosto(CriterioContratacionYUsoDeRigs):
+    def __init__(self):
+        self.plan = {}
+        self._proximo_id_pozo = 1
+
+    def contratar_rigs(self, estado):
+        ''' Agarras el modelo de rig con mejor rendimiento
+        contratas un solo rig
+        lo usas hasta terminar de excavar todo
+        '''
+        if estado.diaNumero != 1:
+            return
+
+        rig_models = estado.configuracion.tiposDeRig
+
+        modelo = min(rig_models, key=lambda m: self._costo_final(m, estado))
+
+        dias_alquiler = max(self._dias_necesarios(modelo, estado.excavacionesActuales),
+                           modelo.diasDeAlquilerMinimo)
+        rig_alquilado = estado.alquilarRIG(modelo, dias_alquiler, 1)
+
+        counter_dia = 1
+        for excavacion in estado.excavacionesActuales:
+            dias = self._dias_necesarios(modelo, [excavacion])
+            for dia in range(counter_dia, counter_dia + dias):
+                self.plan.setdefault(dia, []).append((excavacion, rig_alquilado))
+            counter_dia += dias
+
+    def excavar(self, estado):
+        dia = estado.diaNumero
+
+        if dia in self.plan:
+            plan_dia = self.plan[dia]
+
+            for excavacion, rig in plan_dia:
+
+                excavacion.excavar(rig)
+
+                if excavacion.termino():
+                    nuevo_pozo = Pozo(
+                            excavacion.parcelaPerforada.presionInicial,
+                            self._proximo_id_pozo)
+                    self._proximo_id_pozo += 1
+                    estado.agregarPozo(nuevo_pozo)
+
+            estado.excavacionesActuales = [excavacion for excavacion
+                    in estado.excavacionesActuales if not excavacion.termino]
+
+    # Private helpers
+    def _dias_necesarios(self, modelo, excavaciones):
+        def diasExcavacion(e):
+            r = (100 - e.parcelaPerforada.resistenciaAExcavacion) / 100
+            return math.ceil(e.metrosRestantes / (modelo.metrosDiariosExcavados*r))
+
+        return sum(diasExcavacion(e) for e in excavaciones)
+
+    def _costo_final(self, modelo, estado):
+        costoPorDia = modelo.costoDeAlquilerPorDia + \
+                modelo.consumoDiario * estado.configuracion.costoLitroCombustible
+        dias = max(self._dias_necesarios(modelo, estado.excavacionesActuales),
+                   modelo.diasDeAlquilerMinimo)
+        return dias * costoPorDia
+
