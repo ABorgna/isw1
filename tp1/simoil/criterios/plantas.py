87459ba211c6185ad2479d298a430367d8c137f2
Gonzalo Benegas <gsbenegas@gmail.com>
2017-05-29 01:12:47 -0300

    Agrego criterio plantas maxima capacidad

--- tp1/simoil/criterios/plantas.py
+++ tp1/simoil/criterios/plantas.py
@@ -7,13 +7,30 @@ class CriterioDeConstruccionDePlantasSeparadoras(metaclass=ABCMeta):
     def construir_plantas(self, estado):
         pass
 
+    def potencial_teorico(self, estado):
+        res = 0
+        configuracion = estado.configuracion
+        excavaciones = estado.excavacionesActuales
+        alfa1 = configuracion.alfa1
+        alfa2 = configuracion.alfa2
+
+        def potencial_primer_dia(excavacion, cantidad_habilitada, alfa1, alfa2):
+            ratio = (excavacion.parcelaPerforada.presionInicial /
+                     cantidad_habilitada)
+            return alfa1 * ratio + alfa2 * (ratio ** 2)
+
+        for excavacion in excavaciones:
+            res += potencial_primer_dia(excavacion, len(excavaciones), alfa1,
+                                        alfa2)
+
+        return res
+
+
 class CriterioDeAhorroDePlantas(CriterioDeConstruccionDePlantasSeparadoras):
     def __init__(self):
         self._proximo_id_planta = 1
 
     def construir_plantas(self, estado):
-
-
         '''- con funcion smart ahorro
         elegis modelo con mayor ratio poder / costo
         te fijas cuanto necesitas procesar al primer dia si extraes al maximo
@@ -25,24 +42,40 @@ class CriterioDeAhorroDePlantas(CriterioDeConstruccionDePlantasSeparadoras):
             modelo_mas_rendidor = max(modelos, key=lambda modelo:
                     modelo.volumenDiarioSeparable / modelo.costoDeConstruccion)
 
-            potencial_teorico = 0
-            excavaciones = estado.excavacionesActuales
-            alfa1 = configuracion.alfa1
-            alfa2 = configuracion.alfa2
+            mitad_potencial = math.ceil(self.potencial_teorico(estado) / 2)
 
-            def potencial_primer_dia(excavacion, cantidad_habilitada, alfa1, alfa2):
-                ratio = excavacion.parcelaPerforada.presionInicial / cantidad_habilitada
-                return alfa1 * ratio + alfa2 * (ratio ** 2)
-
-            for excavacion in excavaciones:
-                potencial_teorico += potencial_primer_dia(excavacion, len(excavaciones),
-                        alfa1, alfa2)
-
-            potencial_teorico = math.ceil(potencial_teorico/2)
-            modelos_necesarios = math.ceil(potencial_teorico / \
-                    modelo_mas_rendidor.volumenDiarioSeparable)
+            modelos_necesarios = math.ceil(
+                mitad_potencial / modelo_mas_rendidor.volumenDiarioSeparable)
 
             for i in range(modelos_necesarios):
                 estado.construirPlantaSeparadora(
                         modelo_mas_rendidor, self._proximo_id_planta)
+                self._proximo_id_planta += 1
+
+
+class CriterioPlantasParaMaximaCapacidad(
+        CriterioDeConstruccionDePlantasSeparadoras):
+    def __init__(self):
+        self._proximo_id_planta = 1
+
+    def construir_plantas(self, estado):
+        '''
+        elegis modelo con mayor capacidad de procesamiento
+        te fijas cuanto necesitas procesar al primer dia si extraes al maximo
+        construis hasta cubrir esa necesidad'''
+
+        if estado.diaNumero == 1:
+            configuracion = estado.configuracion
+            modelos = configuracion.tiposDePlantaSeparadora
+            modelo_mayor_capacidad = max(modelos, key=lambda modelo:
+                    modelo.volumenDiarioSeparable)
+
+            potencial = self.potencial_teorico(estado)
+
+            modelos_necesarios = math.ceil(
+                potencial / modelo_mayor_capacidad.volumenDiarioSeparable)
+
+            for i in range(modelos_necesarios):
+                estado.construirPlantaSeparadora(
+                        modelo_mayor_capacidad, self._proximo_id_planta)
                 self._proximo_id_planta += 1
