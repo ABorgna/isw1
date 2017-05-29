c85f895e160d283b927ff1dc0d0ce55075f1edf3
Gonzalo Benegas <gsbenegas@gmail.com>
2017-05-29 00:14:17 -0300

    Agrego criterio parcelas menor profundidad

--- tp1/simoil/criterios/parcelas.py
+++ tp1/simoil/criterios/parcelas.py
@@ -20,3 +20,18 @@ class EleccionParcelasMayorPresion(CriterioEleccionParcelas):
 
             for parcela in parcelas_seleccionadas:
                 estado.agregarExcavacionEnParcela(parcela)
+
+
+class EleccionParcelasMenorProfundidad(CriterioEleccionParcelas):
+    def __init__(self, n_parcelas):
+        self.n_parcelas = int(n_parcelas)
+
+    def decidir_proximas_parcelas(self, estado):
+        if estado.diaNumero == 1:
+            parcelas_seleccionadas = sorted(
+                estado.yacimiento.parcelasDelYacimiento,
+                key=lambda parcela: parcela.profundidad) \
+                [:self.n_parcelas]
+
+            for parcela in parcelas_seleccionadas:
+                estado.agregarExcavacionEnParcela(parcela)
