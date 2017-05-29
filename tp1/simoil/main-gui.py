95cbcf8232455b0c55dfb15bfbde26ec91c70296
Agustin Borgna <hello@aborgna.com.ar>
2017-05-29 02:23:44 -0300

    cosas

--- tp1/simoil/main-gui.py
+++ tp1/simoil/main-gui.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 import remi
 import remi.gui as gui
+
+from estado_de_simulacion import EstadoDeSimulacion
 
 class SimOil(remi.App):
     def __init__(self, *args):
@@ -9,6 +10,9 @@ class SimOil(remi.App):
 
     def main(self):
         self.container = gui.VBox(width = 120, height = 400)
+
+        # Se crea cuando se aprieta el boton
+        self.estado_simulacion = None
 
         self.init_input_container()
         self.init_output_container()
@@ -20,7 +24,7 @@ class SimOil(remi.App):
         self.input_container = gui.Widget(width = 120, height = 400)
         self.container.append(self.input_container)
 
-        self.run_bt = gui.Button('Press me!')
+        self.run_bt = gui.Button('Simular')
 
         # setting the listener for the onclick event of the button
         self.run_bt.set_on_click_listener(self.on_run_button_pressed)
